be0c9d3da7cfdd2d4bb6bb140ae16df8d3d6d6d1
Layna Oyervides <laynaoyervides@gmail.com>
2022-01-23 21:24:30 -0700

    Moving db.json to public directory, renaming to data.json

--- src/components/DeviceItem.js
+++ src/components/DeviceItem.js
@@ -1,8 +1,12 @@
 import React from "react";
-function DeviceItem(){
+import styles from "./DeviceItem.module.css";
+
+function DeviceItem({image, inputs, outputs}){
     return (
-        <div>
-            
+        <div className={styles.device_container}>
+            <div className={styles.input_check}>{inputs}</div>
+            <div className={styles.device_image_render}>{image}</div>
+            <div className={styles.output_check}>{outputs}</div>
         </div>
     );
 }
--- src/components/Practice.js
+++ src/components/Practice.js
@@ -5,7 +5,7 @@ import DeviceItem from "./DeviceItem";
 function Practice (){
     //set up varibale in state
     const [devices, setDevices] = useState([]);
-    //fetch the questions and then assign them to the state variable after the page has loaded
+    //fetch the devices and then assign them to the state variable after the page has loaded
     useEffect(
         () =>
         fetch("http://localhost:3000/devices")
@@ -16,20 +16,15 @@ function Practice (){
     );
 
     const devicePractice = devices.map( (device) => (
-        <DeviceItem key={device.id} device={device.name} image={device.image} inputs={device.input} outputs={device.outputs}/>
+        <DeviceItem key={device.id} device={device.name} image={device.image} inputs={device.inputs} outputs={device.outputs}/>
       ));
-       
-    
-
 
     return (
         <div className={styles.practice_container}>
             <h1>Practice</h1>
             <h2>Use the word bank to choose at least three inputs and outputs of the computing device in the middle</h2>
             {devicePractice}
-            <div className="styles.input_check"></div>
-            <div className="styles.device_image_render"></div>
-            <div className="styles.output_check"></div>
+            
         </div>
     )
 }
