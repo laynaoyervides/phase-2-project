aa17a64501d4f14f88065632455ca84122b569e0
Layna Oyervides <laynaoyervides@gmail.com>
2022-01-30 21:37:40 -0700

    Editing text on Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,8 +5,9 @@ function Home () {
     return (
         <div className={styles.home_container}>
             <h1>What is a Computer?</h1>
-            <h3>A computer is a machine that works with information to help people with thinking tasks</h3>
+            <h3>A computer is a machine that works with information to help people with thinking tasks; all computers input, output, process, and store information. Input and output devices are designed for the computer to receive and communicate information to and from the environment around them.</h3>
             <img src="./iosp.png" alt="IOSP diagram"></img>
+            <h3>Use the Practice page to test your knowledge of inputs and outputs on common computing devices, then, add your own device to the gallery page.</h3>
         </div>
     )
 }
