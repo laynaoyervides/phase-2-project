1452f5e5b2dc57e8c01c2dcae1769ccb04a10790
Layna Oyervides <laynaoyervides@gmail.com>
2022-01-30 22:36:48 -0700

    Added ability to check work onClick event

--- src/components/DeviceItem.js
+++ src/components/DeviceItem.js
@@ -22,7 +22,7 @@ function DeviceItem({device}){
                 <img src={image} alt="device"></img>
           </div>
           <div className="styles.answers">
-              <form onClick={() => setAnswerVisibility((s) => !s)}>
+              <form >
                 <label className="styles.label">
                   <h3>Write your answer here:</h3>
                   <br></br>
@@ -36,11 +36,11 @@ function DeviceItem({device}){
                   placeholder="type here">
                 </textarea>
                 <br></br>
-                <button className="btn" form="guess" type="submit" value="submit">Check your answer!</button>
+                <button onClick={() => setAnswerVisibility((s) => !s)} className="btn" form="guess" type="submit" value="submit">Check your answer!</button>
               </form>
             
             {/* USE AN INLINE STYLE TO SET VISIBILITY TO STATEFUL PROPERTY */}
-            <div style={{ visibility: answerVisibility ? "visibility" : "hidden" }}>
+            <div style={{ visibility: answerVisibility ? "hidden" : "visible" }}>
               <h4>Here is your answer: 
                 <br></br>
                 {body}
